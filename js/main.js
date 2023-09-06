6f2c085f19aedb8f8cb924e812dbe4164b15e621
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-09-06 14:25:39 +0200

    Stampa di 10 indirizzi mail casuali

--- js/main.js
+++ js/main.js
@@ -3,8 +3,28 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      mailList: [],
     };
+  },
+
+  methods: {
+    randomMail() {
+      let i = 0;
+      while (i < 10) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            this.mailList.push(response.data.response);
+            // console.log(response.data.response);
+          });
+        i++;
+      }
+      console.log(this.mailList);
+    },
+  },
+
+  mounted() {
+    this.randomMail();
   },
 }).mount("#app");
 
